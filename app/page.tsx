e5ca5cece706de2440690a87add0c4e4a76c39fe
Arthur Desa <artrdesa@outlook.com>
2025-06-13 16:49:41 -0400

    Updated the front page About Me section based on LinkedIn profile.

--- app/page.tsx
+++ app/page.tsx
@@ -18,44 +18,16 @@ export default function Page() {
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS.
+          Passionate and motivated entry-level full-stack Software Engineer with 
+          a Bachelor of Science degree in Information Technology, with an understanding 
+          of object-oriented programming, data structures, and software engineering best practices. 
         </p>
         <p>
-          Nextfolio has everything you need for a portfolio: MDX blog, SEO, RSS,
-          Atom & JSON feeds, analytics, Tweet & YouTube embeds, KaTeX and {""}
-          <a
-            target="_blank"
-            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
-          >
-            more
-          </a>
-          .
+          Have worked on both the Front End and Back end of web-based projects. Have also made use of API integration. 
         </p>
         <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
-          </a>{" "}
-          and fully customizable, making it easy to add more features.
-        </p>
-        <p>
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
-            target="_blank"
-          >
-            Deploy
-          </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
-          <a href="/blog/getting-started">Getting Started</a> post.
-        </p>
-        <p>
-          Built and maintained by{" "}
-          <a href="https://github.com/ArtDesa" target="_blank">
-            Arthur
-          </a>
-          .
+          Familiar with database systems and database design using SQL and RDBMS. Also familiar with the SDLC, Agile methodology, and Sprints. 
+          Comfortable with the use of version control such a Git and GitHub.
         </p>
       </div>
     </section>
